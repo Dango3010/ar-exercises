4b9032dc1ba69cfa7b2a77618e6b7abe4f0d5c0f
Thi Thuy Oanh Nguyen <thuyoanh199930@gmail.com>
2023-04-23 02:13:15 +0000

    complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,9 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Stores.where(id: 1).first 
+puts @store1
+@store2 = Stores.where(id: 2).first
+puts @store2
+
+@store1.update(name: 'Dango')
