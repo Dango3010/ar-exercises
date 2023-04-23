f7b718cc0d648133fb1d5b4d1cda7647ebd48f63
Thi Thuy Oanh Nguyen <thuyoanh199930@gmail.com>
2023-04-23 03:04:45 +0000

    complete exercise 5

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -6,7 +6,7 @@ puts "----------"
 class Stores < ActiveRecord::Base 
 end
 
-Burnaby = Stores.create(annual_revenue: 300000, mens_apparel: 'yes', womens_apparel: 'yes')
-Richmond = Stores.create(annual_revenue: 1260000, womens_apparel: 'true')
-Gastown = Stores.create(annual_revenue: 190000, mens_apparel: 'true')
+Stores.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: 'yes', womens_apparel: 'yes')
+Stores.create(name: 'Richmond', annual_revenue: 1260000, womens_apparel: 'true')
+Stores.create(name: 'Gastown',annual_revenue: 190000, mens_apparel: 'true')
 puts Stores.count
--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -2,8 +2,27 @@ require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
+require 'pp'
 
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+Stores.create(name: 'Surrey', annual_revenue: 224000, womens_apparel: 'true')
+Stores.create(name: 'whistler', annual_revenue: 1900000, mens_apparel: 'true')
+Stores.create(name: 'yaletown', annual_revenue: 430000, mens_apparel: 'true', womens_apparel: 'true')
+
+@mens_stores = Stores.where(mens_apparel: 'true')
+puts @mens_stores.count 
+
+@mens_stores.each do |store|
+  pp store.name, store.annual_revenue
+end
+
+@womens_stores = Stores.where("annual_revenue < ? AND womens_apparel = ?", 1000000, 'true')
+puts @womens_stores.count
+
+@womens_stores.each do |store|
+  pp store.name, store.annual_revenue
+end
+
+
