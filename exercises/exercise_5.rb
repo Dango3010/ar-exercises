0d22700c9e52651fe75db74e9b1837652cc854eb
Thi Thuy Oanh Nguyen <thuyoanh199930@gmail.com>
2023-04-23 03:19:43 +0000

    complete exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,10 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total = Stores.sum(:annual_revenue)
+average = Stores.average(:annual_revenue)
+
+puts "total: #{total}" 
+puts "average: #{average}"
+
+puts "number of million-dollar stores:", Stores.where("annual_revenue >= ?", 1000000).size 
